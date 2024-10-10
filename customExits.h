905e82c37cf05a409efbbe8c8b0ab5339bd08997
lvbealr <lvbealr@mail.ru>
2024-10-10 03:56:28 +0300

    print errors before exit

--- customExits.h
+++ customExits.h
@@ -4,14 +4,16 @@
 #include <cstdio>
 #include <cstdlib>
 
+#include "stack.h"
+#include "stackDump.h"
+
+extern stack *STACK_PTR;
+
 #define customWarning(expression, returnMessage) do {                               \
     if (!(expression)) {                                                            \
         printf("[WARNING]: %s, file (%s), function (%s), line %d [%s:%d]\n",        \
                 #expression, __FILE__, __FUNCTION__, __LINE__, __FILE__, __LINE__); \
-        STACK->errorStatus |= 1 << returnMessage;                                   \
-        printf("[ERROR BINARY CODE]: ");                                            \
-        printBinaryErrorStatus(STACK->errorStatus);                                 \
-        printf("\n");                                                               \
+        STACK_PTR->errorStatus |= returnMessage;                                    \
         return returnMessage;                                                       \
     }                                                                               \
 } while (0)
@@ -20,10 +22,12 @@
     if (!(expression)) {                                                            \
         printf("[WARNING]: %s, file (%s), function (%s), line %d [%s:%d]\n",        \
                 #expression, __FILE__, __FUNCTION__, __LINE__, __FILE__, __LINE__); \
-        STACK->errorStatus |= 1 << returnMessage;                                   \
+        STACK_PTR->errorStatus |= returnMessage;                                    \
         printf("[ERROR BINARY CODE]: ");                                            \
-        printBinaryErrorStatus(STACK->errorStatus);                                 \
+        printBinaryErrorStatus(STACK_PTR->errorStatus);                             \
         printf("\n");                                                               \
+        printErrorDescription(STACK_PTR->errorStatus);                              \
+        DUMP_(STACK);                                                               \
         exit(0);                                                                    \
         return returnMessage;                                                       \
     }                                                                               \
